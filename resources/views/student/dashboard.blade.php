2ff52c74b06512b32a5e8eb107ebe0c4ec7ec65e
Ayel <annmarielledr@gmail.com>
2025-01-25 00:42:21 +0800

    student dashboard latest layout

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -36,6 +36,40 @@
             </div>
         </div>
     </div>
+    <div class="pending-overview">PENDING APPOINTMENTS</div>
+    <div class="pending-list-sd">
+        <div class="pending-scroll">
+            <div class="pending-container-sd">
+                @for($i = 0; $i < 10; $i++)
+                <div class="pending-wrapper-ovrvw">
+                    <div class="pending-placeholder-sd">
+                        <div class="pending-date-sd">
+                            <span class="date-sd">{{ str_pad(rand(1, 28), 2, '0', STR_PAD_LEFT) }}</span>
+                            <span class="month-sd">{{ ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Oct', 'Jan', 'Oct'][rand(0, 7)] }}</span>
+                        </div>
+                        <div class="pending-details-sd">
+                            <span class="prof-name">Prof. {{ ['Smith', 'Johnson', 'Williams', 'Brown', 'Davis', 'Sanxian', 'Bando', 'Marielle'][rand(0, 7)] }}</span>
+                            <span class="prof-college">College of {{ ['Engineering', 'Arts', 'Agriculture, Food, Environment and Natural Resources', 'Education', 'Criminal Justice', 'Economics, Management and Development Studies', 'Science'][rand(0, 6)] }}</span>
+                            
+                        </div>
+                        <div class="pending-time-sd">
+                            <span class="prof-time">{{ str_pad(rand(8, 16), 2, '0', STR_PAD_LEFT) }}:00 {{ rand(0, 1) ? 'AM' : 'PM' }}</span>
+                            <span class="prof-duration">30 Min</span>
+                        </div>
+                    </div>
+                        <button class="cancel-apt-sd" onclick="cancelAppointment(this)">&times;</button>
+                </div>
+                    <div class="no-app-yet">No approved appointments</div>
+                @endfor
+            </div>
+        </div>
+    </div>
+    <div class="no-book">
+        <p>Make an appointment </p>
+    </div>
+    <div class="apt">
+        <button class="apt-here-button" onclick="window.location.href='/faculty-availability'">Book Now</button>
+    </div>
 </div>
 
 <script>
